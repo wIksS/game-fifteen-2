982fdb95965b2853d5bb720fb3ffdb2e571ce8d4
bhinova <bhinova@gmail.com>
2014-07-08 16:16:32 +0300

    Added GameFifteen-sceleton
    Добавила съм примерен скелет. Чувствайте се свободни да давате идеи за
    подобряването му.

---
+++ GameFifteen/GameFifteen.Common/MatrixGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFifteen.Common
+{
+    public class MatrixGenerator
+    {
+        public const int MatrixLength = 4;
+
+        public bool IfOutOfMAtrix(int row, int col)
+        {
+            if (row >= MatrixLength || row < 0 || col < 0 || col >= MatrixLength)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GameFifteen/GameFifteen.CommonTests/MatrixGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameFifteen.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace GameFifteen.Common.Tests
+{
+    [TestClass()]
+    public class MatrixGeneratorTests
+    {
+        [TestMethod()]
+        public void IfOutOfMAtrixTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
