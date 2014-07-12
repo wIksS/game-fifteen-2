43fed50147907a3c4633b6501266ed5a135dfb6b
juliameleshko <julia.meleshko@yahoo.com>
2014-07-12 15:59:08 +0300

    some refactor of 'MatrixEmptyCellRandomizator.cs'

--- GameFifteen/GameFifteen.Common/MatrixEmptyCellRandomizator.cs
+++ GameFifteen/GameFifteen.Common/MatrixEmptyCellRandomizator.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GameFifteen.Common.Utils;
-
-namespace GameFifteen.Common
+﻿namespace GameFifteen.Common
 {
+    using GameFifteen.Common.Utils;
+
     class MatrixEmptyCellRandomizator
     {
-        private int[] dirR = new int[4] { -1, 0, 1, 0 };
-        private int[] dirC = new int[4] { 0, 1, 0, -1 };
-        private Point emptyPoint = new Point(3,3);
+        private const int GAME_DIRECTIONS_COUNT = 4;
+        private const int INIT_POINT_POSITION = 3;
+        private const int MIN_RANDOM_NUMBER = 10;
+        private const int MAX_RANDOM_NUMBER = 21;
+        private const int MAX_RANDOM_DIRECTION_NUMBER = 4;
+
+        private int[] directionRow = new int[GAME_DIRECTIONS_COUNT] { -1, 0, 1, 0 };
+        private int[] directionColumn = new int[GAME_DIRECTIONS_COUNT] { 0, 1, 0, -1 };
+        private Point emptyPoint = new Point(INIT_POINT_POSITION, INIT_POINT_POSITION);
 
         public Point Randomize(int[,] matrix)
         {
-            int randomizeMoves = RandomUtils.GetRandomNumber(10, 21);
+            int randomizeMoves = RandomUtils.GetRandomNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
             for (int i = 0; i < randomizeMoves; i++)
             {
-                int randomDirection = RandomUtils.GetRandomNumber(4);
-                Point newEmptyPoint = new Point(emptyPoint.Row + dirR[randomDirection], emptyPoint.Col + dirC[randomDirection]);
+                int randomDirection = RandomUtils.GetRandomNumber(MAX_RANDOM_DIRECTION_NUMBER);
+                Point newEmptyPoint = new Point(emptyPoint.Row + directionRow[randomDirection], emptyPoint.Col + directionColumn[randomDirection]);
 
                 if (OutOfMatrixChecker.CheckIfOutOfMatrix(newEmptyPoint, matrix.GetLength(0)))
                 {
