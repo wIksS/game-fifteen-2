ae558e86833d467a0515cd307c6cbbf8eb46a6bc
Viktor <viktordakov97@gmail.com>
2014-07-25 09:12:56 +0300

    Refactored the ConsoleRenderer class. Now it works simplier. Refactored the dashes in the PrintMatrix so their number isnt hardcoded 12 but it is the matrix length multiplied by the intervals in the matrix renderer.

--- GameFifteen/GameFifteen.Common/UI/ConsoleRenderer.cs
+++ GameFifteen/GameFifteen.Common/UI/ConsoleRenderer.cs
@@ -11,39 +11,50 @@
         public void RenderMatrix(int[,] matrix)
         {
             string dashes = UIConstants.SPACE + new string(UIConstants.DASH, 12);
-            var matrixAsString = new StringBuilder();
+            int matrixLength = matrix.GetLength(0);
+            StringBuilder matrixAsString = new StringBuilder();
 
             matrixAsString.AppendLine(dashes);
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int row = 0; row < matrixLength; row++)
             {
                 matrixAsString.Append(UIConstants.WALL_SYMBOL);
-                for (int j = 0; j < matrix.GetLength(0); j++)
+                for (int col = 0; col < matrixLength; col++)
                 {
-                    if (matrix[i, j] <= 9)
-                    {
-                        matrixAsString.AppendFormat(UIConstants.FIRST_PLACEHOLDER, matrix[i, j]);
-                    }
-                    else
-                    {
-                        if (matrix[i, j] == 16)
-                        {
-                            matrixAsString.Append(UIConstants.EMPTY_SPACES);
-                        }
-                        else
-                        {
-                            matrixAsString.AppendFormat(UIConstants.SECOND_PLACEHOLDER, matrix[i, j]);
-                        }
-                    }
-
-                    if (j == matrix.GetLength(0) - 1)
-                    {
-                        matrixAsString.AppendFormat(UIConstants.WALL_SYMBOL, UIConstants.NEW_LINE);
-                    }
+                    matrixAsString.AppendFormat("{0,3}",matrix[row,col]);
                 }
-
-                matrixAsString.AppendLine();
+                matrixAsString.AppendLine(UIConstants.WALL_SYMBOL);
             }
+
+            //for (int i = 0; i < matrix.GetLength(0); i++)
+            //{
+            //    matrixAsString.Append(UIConstants.WALL_SYMBOL);
+            //    for (int j = 0; j < matrix.GetLength(0); j++)
+            //    {
+            //        if (matrix[i, j] <= 9)
+            //        {
+            //            matrixAsString.AppendFormat(UIConstants.FIRST_PLACEHOLDER, matrix[i, j]);
+            //        }
+            //        else
+            //        {
+            //            if (matrix[i, j] == 16)
+            //            {
+            //                matrixAsString.Append(UIConstants.EMPTY_SPACES);
+            //            }
+            //            else
+            //            {
+            //                matrixAsString.AppendFormat(UIConstants.SECOND_PLACEHOLDER, matrix[i, j]);
+            //            }
+            //        }
+
+            //        if (j == matrix.GetLength(0) - 1)
+            //        {
+            //            matrixAsString.AppendFormat(UIConstants.WALL_SYMBOL, UIConstants.NEW_LINE);
+            //        }
+            //    }
+
+            //    matrixAsString.AppendLine();
+            //}
 
             matrixAsString.AppendLine(dashes);
 
