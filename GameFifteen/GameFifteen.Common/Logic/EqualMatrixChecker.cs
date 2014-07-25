3ee3b37deb29489c317f8f2e1dbce4a9b3c7e316
dany90 <dany90@gmail.com>
2014-07-25 13:57:39 +0300

    Implemented exeption when null objects are passed to IsSorted method

--- GameFifteen/GameFifteen.Common/Logic/EqualMatrixChecker.cs
+++ GameFifteen/GameFifteen.Common/Logic/EqualMatrixChecker.cs
@@ -1,12 +1,19 @@
 ﻿namespace GameFifteen.Common
 {
     using GameFifteen.Common.Contracts;
+    using System;
 
     public class EqualMatrixChecker : IEqualMatrixChecker
     {
         public bool IsSorted(int[,] currentMatrix)
         {
+            if (currentMatrix==null)
+            {
+                throw new ArgumentNullException("The matrix cannot be null or empty");
+            }
+
             int matrixSize=currentMatrix.GetLength(0);
+
             INumberGenerator numberGenerator=new NumberGenerator(matrixSize*matrixSize);
             IMatrixGenerator matrixGenerator = new MatrixGenerator(matrixSize, numberGenerator);
             int[,] sortedMatrix = matrixGenerator.GenerateMatrix();
