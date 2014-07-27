bcb518aa2e8834418d64858dfecf9752f926d867
juliameleshko <julia.meleshko@yahoo.com>
2014-07-27 22:04:25 +0300

    xml documentation for Utils

--- GameFifteen/GameFifteen.Common/Utils/Directions.cs
+++ GameFifteen/GameFifteen.Common/Utils/Directions.cs
@@ -4,8 +4,10 @@
     using System.Linq;
     using GameFifteen.Common;
 
+    /// <summary>Represents directions.</summary>
     public static class Directions
     {
+        /// <summary>The possibe directions.</summary>
         public static readonly Point[] GetDirection = { new Point(0, 1), new Point(0, -1), new Point(1, 0), new Point(-1, 0) };
     }
 }
--- GameFifteen/GameFifteen.Common/Utils/RandomGenerator.cs
+++ GameFifteen/GameFifteen.Common/Utils/RandomGenerator.cs
@@ -2,30 +2,31 @@
 {
     using System;
 
+    /// <summary>Represents a random generator.</summary>
     public class RandomGenerator
     {
         private static Random numberInMatrix = new Random();
 
         // unused
-        /// <summary>
-        /// Returns a nonnegative random integer.
-        /// </summary>
+        /// <summary>Gets random number.</summary>
+        /// <returns>The random nonnegative number.</returns>
         public static int GetRandomNumber()
         {
             return numberInMatrix.Next();
         }
 
-        /// <summary>
-        /// Returns a random number in the range [0, max] (inqluding)
-        /// </summary>
+        /// <summary>Gets random number.</summary>
+        /// <param name="max" type="int">The maximum of the range.</param>
+        /// <returns>The random number in the range [0, max] (including).</returns>
         public static int GetRandomNumber(int max)
         {
             return numberInMatrix.Next(max+1);
         }
 
-        /// <summary>
-        /// Returns a random number in the range [min, max] (inqluding)
-        /// </summary>
+        /// <summary>Gets random number.</summary>
+        /// <param name="min" type="int">The minimum of the range.</param>
+        /// <param name="max" type="int">The maximum of the range.</param>
+        /// <returns>The random number in the range [min, max] (including).</returns>
         public static int GetRandomNumber(int min, int max)
         {
             return numberInMatrix.Next(min, max + 1);
