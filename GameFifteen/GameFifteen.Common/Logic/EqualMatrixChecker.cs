a8bbc117462798883a97622b6da9d2eaf230eb0b
dany90 <dany90@gmail.com>
2014-07-27 12:24:22 +0300

    Minor refactoring of EqualMatrixChecker. Reusing matrixSize.

--- GameFifteen/GameFifteen.Common/Logic/EqualMatrixChecker.cs
+++ GameFifteen/GameFifteen.Common/Logic/EqualMatrixChecker.cs
@@ -8,20 +8,20 @@
     {
         public bool IsSorted(int[,] currentMatrix)
         {
-            if (currentMatrix==null)
+            if (currentMatrix == null)
             {
                 throw new ArgumentNullException("The matrix cannot be null or empty");
             }
 
-            int matrixSize=currentMatrix.GetLength(0);
+            int matrixSize = currentMatrix.GetLength(0);
 
-            INumberGenerator numberGenerator=new NumberGenerator(matrixSize*matrixSize);
+            INumberGenerator numberGenerator = new NumberGenerator(matrixSize * matrixSize);
             IMatrixGenerator matrixGenerator = new MatrixGenerator(matrixSize, numberGenerator);
             int[,] sortedMatrix = matrixGenerator.GenerateMatrix();
 
-            for (int i = 0; i < sortedMatrix.GetLength(0); i++)
+            for (int i = 0; i < matrixSize; i++)
             {
-                for (int j = 0; j < sortedMatrix.GetLength(0); j++)
+                for (int j = 0; j < matrixSize; j++)
                 {
                     if (currentMatrix[i, j] != sortedMatrix[i, j])
                     {
