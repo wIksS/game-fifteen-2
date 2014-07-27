ce0f2a991591c658d2d46df843816d9726a18661
juliameleshko <julia.meleshko@yahoo.com>
2014-07-27 22:07:15 +0300

    xml documentation for EntryPoint

--- GameFifteen/GameFifteen.Common/EntryPoint.cs
+++ GameFifteen/GameFifteen.Common/EntryPoint.cs
@@ -4,11 +4,13 @@
     using GameFifteen.Contracts;
     using GameFifteen.Logic;
 
+    /// <summary>Represents the entry point.</summary>
     public class Program
     {
+        /// <summary>Main entry-point for this application.</summary>
+        /// <param name="args" type="string[]">Array of command-line argument strings.</param>
         static void Main(string[] args)
         {
-
             GameEngine gameEngine = new GameEngine();
 
             do
